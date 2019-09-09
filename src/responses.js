359e107ff9662e3862f8cf095f5a0fa7207d21db
Celeste Yost <17mcyost@gmail.com>
2019-09-09 15:03:10 -0400

    demo dun

--- src/responses.js
+++ src/responses.js
@@ -1,4 +1,4 @@
-const fs = require('fs');  // pull in the file system module
+const fs = require('fs'); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
@@ -10,6 +10,25 @@ const respond = (request, response, content, type) => {
 
 const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
+};
+
+const getCats = (request, response, acceptedTypes) => {
+  const cat = {
+    name: 'Garfield',
+    age: 6,
+  };
+
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <name>${cat.name}</name>`;
+    responseXML = `${responseXML} <age>${cat.age}</age>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request, response, responseXML, 'text/xml');
+  }
+
+  const catString = JSON.stringify(cat);
+  return respond(request, response, catString, 'application/json');
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -5,11 +5,24 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-
+  '/': responseHandler.getIndex,
+  '/cats': responseHandler.getCats,
+  index: responseHandler.getIndex,
 };
 
 const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  // make accepted data file types into array
+  const acceptedTypes = request.headers.accept.split(',');
 
+
+  // send request to a handler
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, acceptedTypes);
+  } else {
+    // could put a 404 page here hehe
+    urlStruct.index(request, response, acceptedTypes);
+  }
 };
 
 http.createServer(onRequest).listen(port);
